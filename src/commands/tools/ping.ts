9e2e2f23002e11f08e03eb0028ba0323dc4a8f39
YinCheng0106 <eason010630@gmail.com>
2025-10-30 21:46:58 +0800

    feat: add Supabase dependencies and update ping command for ephemeral replies

--- src/commands/tools/ping.ts
+++ src/commands/tools/ping.ts
@@ -1,12 +1,17 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, MessageFlags } from "discord.js";
 import type { CommandInteraction } from "discord.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
-    
-  async execute(interaction : CommandInteraction) {
-    await interaction.reply("Pong!");
-  }
-}
+
+  async execute(interaction: CommandInteraction) {
+    await interaction.deferReply({
+      flags: MessageFlags.Ephemeral,
+    });
+    await interaction.editReply({
+      content: "Pong!",
+    });
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import dotenv from "dotenv";
-import { Client, GatewayIntentBits, Collection } from "discord.js";
+import {
+  ActivityType,
+  Client,
+  GatewayIntentBits,
+  Collection,
+} from "discord.js";
 import { handleCommands } from "@/functions/handlers/handleCommands";
 import { handleEvents } from "@/functions/handlers/handleEvents";
 
 dotenv.config();
 
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.GuildMembers,
-  GatewayIntentBits.MessageContent,
-] }) as Client & {
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+}) as Client & {
   commands?: Collection<string, any>;
 };
 
 (async () => {
   await handleCommands(client);
   await handleEvents(client);
-  client.login(process.env.BOT_TOKEN);
+  client.login(process.env.BOT_TOKEN).then(() => {
+    client.user?.setActivity("正在讓 每個人成為勇者", {
+      type: ActivityType.Custom,
+    });
+  });
 })();
