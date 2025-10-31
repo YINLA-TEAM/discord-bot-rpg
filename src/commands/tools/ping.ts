3b5f36bb4cd1cc51ad4eb7f033f091d3212bc96f
YinCheng0106 <eason010630@gmail.com>
2025-10-31 14:49:36 +0800

    feat: update ping command to include latency response and localization support

--- src/commands/tools/ping.ts
+++ src/commands/tools/ping.ts
@@ -4,14 +4,20 @@ import type { CommandInteraction } from "discord.js";
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setNameLocalizations({
+      "zh-TW": "延遲",
+    })
+    .setDescription("查看機器人延遲"),
 
   async execute(interaction: CommandInteraction) {
+    const startTime = Date.now();
     await interaction.deferReply({
       flags: MessageFlags.Ephemeral,
     });
+    const endTime = Date.now();
+    const latency = endTime - startTime;
     await interaction.editReply({
-      content: "Pong!",
+      content: `# \`${latency}\` ms`,
     });
   },
 };
